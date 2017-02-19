2064773807608f4f5272cb0986dddc252fb13113
vidalCreates <amc12vidal@hotmail.com>
2017-02-18 20:51:09 -0500

    FFT progess > retrieving output from method

--- FindThatSpeaker/MainActivity.cs
+++ FindThatSpeaker/MainActivity.cs
@@ -5,6 +5,7 @@ using Android.App;
 using Android.Widget;
 using Android.OS;
 using Android.Media;
+using System.Collections;
 
 namespace FindThatSpeaker
 {
@@ -12,7 +13,7 @@ namespace FindThatSpeaker
     public class MainActivity : Activity
     {
         string filePath = "";
-        string filename = "testAudio.mp3";
+        string filename = "testAudio.wav";
 
         ImageButton startRecordButton;
 
@@ -33,7 +34,7 @@ namespace FindThatSpeaker
             filePath = Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryMusic) + "/" + filename;
 
             log = FindViewById<TextView>(Resource.Id.log);
-            log.Text = "-Log-\n\n-filepath-\n" + filePath + "\n\nwaiting...";
+            log.Text = "-Log-\n\nwaiting...";
 
             startRecordButton.Click += StartRecord;
         }
@@ -43,7 +44,7 @@ namespace FindThatSpeaker
             startRecordButton.Enabled = false;//disable button
            // startRecordButton.Text = "recording";
 
-            log.Text = "-Log-\n\n-filepath-\n"+ filePath + "\n\nrecording...";//update the log
+            log.Text = "-Log-\n\nrecording...";//update the log
 
             try
             {
@@ -85,14 +86,55 @@ namespace FindThatSpeaker
         
         private void StopRecord(object sender, System.EventArgs e)
         {
-            log.Text = "-Log-\n\n-filepath-\n" + filePath + "\n\nrecording stopped...";//update the log
+            log.Text = "-Log-\n\nwaiting...";//update the log
            // startRecordButton.Text = "Tap to record";
             recorder.Stop();
             recorder.Reset();
             recorder.Release();
             recorder = null;
             startRecordButton.Enabled = true;//re-enable button
+            TestFFT();
         }
-        
+
+        private byte[] GetFileBytes(string filename)
+        {
+            byte[] bytes;
+            using (FileStream file = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            {
+                bytes = new byte[file.Length];
+                file.Read(bytes, 0, (int)file.Length);
+            }
+            /*BitArray bits = new BitArray(bytes);
+            for (int i = 0; i < bits.Count; i++)
+            {
+                bool bit = bits.Get(i);
+                Console.Write(bit ? 1 : 0);
+            }*/
+            return bytes;
+        }
+
+        private void TestFFT()
+        {
+            byte[] data = GetFileBytes(filePath);
+            double[] x = new double[data.Length];
+            double[] y = new double[data.Length];
+
+            //converting byte array into double array
+            for (int i = 0; i < x.Length; i++)
+            {
+                x[i] = data[i] / 32768.0;
+            }
+
+            //display contents of input arrays
+            log.Text = "realin: " + x[0] + "\n";
+            log.Text += "imagin: " + y[0] + "\n\n";
+
+            Fft.Transform(x, y);//perform fourier fast transform
+
+            //display contents of output arrays
+            log.Text += "realout: " + x[0] + "\n";
+            log.Text += "imagout: " + y[0];
+        }
+
     }
 }
